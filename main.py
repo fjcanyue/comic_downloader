74a714fd0ba9dce57c91211537388fe6be7f7493
fjcanyue <fjcanyue@163.com>
2022-04-28 17:55:05 +0800

    fix: add signal handling to catch ctrl+c event

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+import signal
 import sys
 
 from downloader.shell import Shell
 
-if __name__ == '__main__':
+
+def main():
+    signal.signal(signal.SIGINT, __handler__)
     args = sys.argv[1:]
     if len(args) == 0:
         path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -12,3 +15,12 @@ if __name__ == '__main__':
         output_path = args[0]
 
     Shell(output_path).cmdloop()
+
+
+def __handler__(signum, frame):
+    print('感谢使用，再会！')
+    exit(1)
+
+
+if __name__ == '__main__':
+    main()
